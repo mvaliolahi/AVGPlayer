f9b5dfd919bd3df3750645d66a73de52ec1105a3
meysam valiolahi <mvaliolahi@gmail.com>
2018-03-29 09:58:42 +0430

    Initial.

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View >
+          <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
